1f663e3364dada95c2a7b5acef48a4993da7b1e3
PLUCK823 <92411872+PLUCK823@users.noreply.github.com>
2025-03-31 06:59:39 +0800

    添加后端应用程序启动类

--- src/main/java/org/hz/backend/BackendApplication.java
+++ src/main/java/org/hz/backend/BackendApplication.java
@@ -2,12 +2,15 @@ package org.hz.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+/**
+ * 后端应用程序启动类
+ */
 @SpringBootApplication
 @EnableTransactionManagement
-@EnableScheduling
+@EnableConfigurationProperties
 public class BackendApplication {
 
     public static void main(String[] args) {
